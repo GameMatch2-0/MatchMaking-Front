f84a0bea57f9c5c9979aa026d50da2e76b7d1144
Silva <maria.e.r.silva@accenture.com>
2023-12-03 21:52:09 -0300

    criacao pagina de contato

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,24 @@
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
+import "../css/contato.css"
+import React from "react";
+
+export default function Contato(){
+    return(
+        <>
+        <Navbar />
+
+       
+                <div className="div-contato">
+                    <h1>Fale com a gente!</h1>
+                    <div className="text-div">
+                        <textarea cols="30" rows="10" className="txt-area"></textarea>
+                    </div>
+                    <button>Enviar</button>
+                    <p>Tente também whatsapp 219821218937219</p>
+                </div>
+            
+        <Footer />
+        </>
+    )
+}
