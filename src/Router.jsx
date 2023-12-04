dda2cf0aa6587cdf1661a0b6786c6c4a0e1d0269
Silva <maria.e.r.silva@accenture.com>
2023-12-03 21:37:11 -0300

    criacao da pagina planos

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Index from './pages/Index'
+import Planos from './pages/planos'
 import Profile from './pages/Profile'
 
 const routes = createBrowserRouter([
   { path: '/', element: <Index /> },
-//  { path: '/planos', element: <Planos /> },
+  { path: '/planos', element: <Planos /> },
 //  { path: '/sobre', element: <Sobre /> },
 //  { path: '/contato', element: <Contato /> },
   { path: '/profile', element: <Profile /> },
---
+++ src/pages/Planos.jsx
@@ -0,0 +1,26 @@
+import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
+import beneficios from "../assets/beneficios.png"
+import "../css/planos.css"
+import React from "react";
+
+export default function Planos(){
+    return(
+        <>
+        <Navbar />
+
+        <div className="div-planos">
+                <div className="planos-div-direita">
+                    <h4 className="planos-info">MatchMaking Premium, mais liberdade para você ser quem quiser!</h4>
+                    <h1>Teste por 1 mês GRATIS - Depois R$19,90</h1>
+                    <button>Assinar</button>
+                    <p>Vamos enviar um lembrete 7 dias antes de o teste terminar. Faturamento recorrente.</p>
+                </div>
+                <div className="planos-div-esquerda">
+                    <img src={beneficios} alt="" />
+                </div>
+            </div>
+        <Footer />
+        </>
+    )
+}
